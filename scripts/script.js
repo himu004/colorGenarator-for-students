0dc41b9010c02882865d580f98a585c48f343aee
Himu004 <himu.chowdhury01@gmail.com>
2024-09-16 15:36:39 +0600

    Color Generator for Students Project Added

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+// Random Color Generator function
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+console.log(randomColor());
+
+const btnStart = document.getElementById("btn-start");
+const btnStop = document.getElementById("btn-stop");
+
+let intervalID;
+const startChangingColor = function () {
+    intervalID = setInterval(changBgAuto, 500);
+function changBgAuto() {
+    const boxBody = document.getElementById('color-box');
+    boxBody.style.backgroundColor = randomColor();
+    // ! MY LOGIC
+    const p = document.getElementById('color-box');
+    p.innerText = randomColor();
+}
+
+};
+const stopChangingColor = function () {
+    clearInterval(intervalID)
+};
+
+btnStart.addEventListener("click", startChangingColor);
+
+btnStop.addEventListener("click", stopChangingColor);
